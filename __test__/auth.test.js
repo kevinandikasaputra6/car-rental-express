ee9052a9b04919670d90049c07070ab38b5e678f
Kevin2606-code <kevinandika897@gmail.com>
2024-10-17 13:17:36 +0700

    update

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,7 +1,5 @@
 const request = require("supertest");
 const server = require("../index");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 
 const testUser = {
   email: "test@test.com",
@@ -16,12 +14,12 @@ describe("POST ?api/v1/signup", () => {
       .send(testUser)
       .set("Accept", "application/json")
       .then((res) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).objectContaining({
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual({
           code: 200,
           status: "success",
           message: "Sign up successfully",
-          data: expext.objectContaining({
+          data: expect.objectContaining({
             user: {
               email: "test@test.com",
               password: expect.not.stringContaining("Password1!"),
@@ -34,9 +32,9 @@ describe("POST ?api/v1/signup", () => {
               phone_number: null,
               role_id: 3,
               create_by: null,
-              create_dt: expect.any(string),
+              create_dt: expect.any(String),
               update_by: null,
-              update_at: expect.any(string),
+              update_at: expect.any(String),
             },
           }),
         });
@@ -51,4 +49,46 @@ describe("POST ?api/v1/signup", () => {
   it("should response with 400 status code", () => {
     // it adalah expectasi kita
   });
+});
+
+describe("POST ?api/v1/signin", () => {
+  it("should response with 200 status code", (done) => {
+    request(server)
+      .post("/api/v1/auth/signin")
+      .send(testUser)
+      .set("Accept", "application/json")
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({
+          code: 200,
+          status: "success",
+          message: "Sign in successfully",
+          data: expect.objectContaining({
+            token: expect.any(String),
+            user: {
+              email: "test@test.com",
+              password: expect.not.stringContaining("Password1!"),
+              address: null,
+              avatar: null,
+              birth_date: null,
+              driver_license: null,
+              fullname: null,
+              gender: null,
+              phone_number: null,
+              role_id: 3,
+              create_by: null,
+              create_dt: expect.anything(),
+              update_by: null,
+              update_at: expect.anything(),
+            },
+          }),
+        });
+        done();
+      })
+      .catch((e) => {
+        console.log(e);
+        done();
+      });
+  });
+  it("should response with 400 status code", () => {});
 });
